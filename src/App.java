676ae94df16325dd5b90433aa11b722dd5de34ab
sdoste <sergiodoste@gmail.com>
2024-05-07 18:14:13 +0200

    with arraylistloop

--- src/App.java
+++ src/App.java
@@ -1,5 +1,9 @@
+import java.util.ArrayList;
+import java.util.Collections;
+
 public class App {
     public static void main(String[] args) throws Exception {
+        ArrayList<Juguete> jugueteria =  new ArrayList<>();
         Juguete juguete1 = new Juguete("Pelota", 10.99, new Dimension(5.0, 5.0, 5.0));
         Juguete juguete2 = new Juguete("Muñeca", 15.50, new Dimension(10.0, 15.0, 5.0));
         Juguete juguete3 = new Juguete("Rompecabezas", 20.75, new Dimension(15.0, 10.0, 3.0));
@@ -10,17 +14,11 @@ public class App {
         Juguete juguete8 = new Juguete("Set de Cocina de Juguete", 22.50, new Dimension(15.0, 10.0, 8.0));
         Juguete juguete9 = new Juguete("Avión de Juguete", 28.99, new Dimension(10.0, 5.0, 4.0));
         Juguete juguete10 = new Juguete("Bicicleta de niño", 40.50, new Dimension(50.0, 95.0, 17.0));
+        Collections.addAll(jugueteria, juguete1, juguete2, juguete3, juguete4, juguete5, juguete6, juguete7, juguete8, juguete9, juguete10);
 
-        System.out.println(juguete1.toString());
-        System.out.println(juguete2.toString());
-        System.out.println(juguete3.toString());
-        System.out.println(juguete4.toString());
-        System.out.println(juguete5.toString());
-        System.out.println(juguete6.toString());
-        System.out.println(juguete7.toString());
-        System.out.println(juguete8.toString());
-        System.out.println(juguete9.toString());
-        System.out.println(juguete10.toString());
+       for (int i = 0; i < jugueteria.size(); i++ ){
+        System.out.println(jugueteria.get(i).toString() + "\n");
+       }
 
 
     }
